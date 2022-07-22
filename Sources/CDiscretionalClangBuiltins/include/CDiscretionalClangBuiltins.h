7cb0c44f99295d0de42ca2fc32a7a3c4eca3af84
Gwynne Raskind <gwynne@vapor.codes>
2022-07-22 05:57:09 -0500

    Update builtins header for more modern Clang

--- Sources/CDiscretionalClangBuiltins/include/CDiscretionalClangBuiltins.h
+++ Sources/CDiscretionalClangBuiltins/include/CDiscretionalClangBuiltins.h
@@ -12,25 +12,35 @@ struct ClangBuiltins {};
 // for the builtin itself so we can mark it unavailable in Swift (otherwise, the Clang importer insists
 // on noticing the implicit declaration it was effectively given by being referenced without a
 // preexisting prototype and making it publicly callable in Swift).
-#define MultiprecisionMathBuiltin(n, t) \
-__attribute__((swift_name("ClangBuiltins."#n"(x:y:carryin:carryout:)"))) static inline t n(t x, t y, t ci, t *co) { return __builtin_ ## n(x, y, ci, co); } \
-extern t __attribute__((availability(swift,unavailable))) __builtin_ ## n(t, t, t, t *);
+#define MultiprecisionMathBuiltin(name, basetype) \
+    __attribute__((swift_name("ClangBuiltins."#name"(x:y:carryin:carryout:)"))) \
+    static inline basetype name(basetype x, basetype y, basetype ci, basetype *co) { \
+        return __builtin_ ## name(x, y, ci, co); \
+    } \
+    extern basetype \
+        __attribute__((availability(swift,unavailable))) \
+        __builtin_ ## name(basetype, basetype, basetype, basetype *);
 
 // Same thing, only the rotate builtins need to be renamed, and they take less parameters.
-#define BitwiseRotationBuiltin(n, sn, t) \
-__attribute__((swift_name("ClangBuiltins."#sn"(x:y:)"))) static inline t n(t x, t y) { return __builtin_ ## n(x, y); } \
-extern t __attribute__((availability(swift,unavailable))) __builtin_ ## n(t, t);
+#define BitwiseRotationBuiltin(name, swiftname, basetype) \
+    __attribute__((swift_name("ClangBuiltins."#swiftname"(x:y:)"))) \
+    static inline basetype name(basetype x, basetype y) { \
+        return __builtin_ ## name(x, y); \
+    } \
+    extern basetype \
+        __attribute__((availability(swift,unavailable))) \
+        __builtin_ ## name(basetype, basetype);
 
 MultiprecisionMathBuiltin(addcb, uint8_t)
 MultiprecisionMathBuiltin(addcs, uint16_t)
 MultiprecisionMathBuiltin(addc, uint32_t)
 MultiprecisionMathBuiltin(addcl, uintptr_t)
-MultiprecisionMathBuiltin(addcll, uint64_t)
+MultiprecisionMathBuiltin(addcll, unsigned long long)
 MultiprecisionMathBuiltin(subcb, uint8_t)
 MultiprecisionMathBuiltin(subcs, uint16_t)
 MultiprecisionMathBuiltin(subc, uint32_t)
 MultiprecisionMathBuiltin(subcl, uintptr_t)
-MultiprecisionMathBuiltin(subcll, uint64_t)
+MultiprecisionMathBuiltin(subcll, unsigned long long)
 
 BitwiseRotationBuiltin(rotateleft8, rolb, uint8_t)
 BitwiseRotationBuiltin(rotateleft16, rols, uint16_t)
@@ -39,14 +49,7 @@ BitwiseRotationBuiltin(rotateleft64, rolll, uint64_t)
 BitwiseRotationBuiltin(rotateright8, rorb, uint8_t)
 BitwiseRotationBuiltin(rotateright16, rors, uint16_t)
 BitwiseRotationBuiltin(rotateright32, ror, uint32_t)
-
-// https://reviews.llvm.org/D67606
-// The version of Clang in use at the time of this writing (Apple clang version 11.0.3 (clang-1103.0.32.29,
-// from Xcode 11.4) does not have this fix yet. Hopefully the next release of Xcode will have it.
-//BitwiseRotationBuiltin(rotateright64, rorll, uint64_t)
-__attribute__((swift_name("ClangBuiltins.rorll(x:y:)")))
-static inline uint64_t _rotateright64(uint64_t x, uint64_t y) { return __builtin_rotateright64(x, y); }
-extern uint64_t __attribute__((availability(swift,unavailable))) __builtin_rotateright64(uint64_t, int64_t);
+BitwiseRotationBuiltin(rotateright64, rorll, uint64_t)
 
 #undef BitwiseRotationBuiltin
 #undef MultiprecisionMathBuiltin
